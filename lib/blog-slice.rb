d4e46a683467e6f324b0163ee49f0f7efd208ebc
Maxime Guilbot <maxime@ekohe.com>
2009-01-06 01:03:44 +0100

    Fix a bug, sorry scott

--- lib/blog-slice.rb
+++ lib/blog-slice.rb
@@ -2,7 +2,7 @@ if defined?(Merb::Plugins)
 
   $:.unshift File.dirname(__FILE__)
 
-  load_dependency 'merb-slices'
+  dependency 'merb-slices'
   Merb::Plugins.add_rakefiles "blog-slice/merbtasks", "blog-slice/slicetasks", "blog-slice/spectasks"
 
   # Register the Slice for the current host application
